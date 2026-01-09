27562a72e599e363cf33da6b77f98d7c2bb48d2d
FluffyKn1ght <lightcraftzofficial@gmail.com>
2026-01-09 22:52:25 +0300

    Add formatting via black + port over vgm.py

--- mdfmvgmrip.py
+++ mdfmvgmrip.py
@@ -11,176 +11,190 @@ from vgm import VGM, VGMError
 from ym2612 import YM2612, YM2612Error, YM2612Instrument
 
 
-DEBUG : bool = "--debug" in sys.argv
+DEBUG: bool = "--debug" in sys.argv
+
 
 def dump_datablocks(data: dict, out_dir: str) -> None:
-  try:
-    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
-  except Exception as e:
-    raise IOError(f"Unable to create FM instrument output folder: {e}")
+    try:
+        pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        raise IOError(f"Unable to create FM instrument output folder: {e}")
 
-  sections : list[bytes] = []
+    sections: list[bytes] = []
 
-  for section in data[0x00]["sections"]:
-    sections.append(data[0x00]["data"][section["start"]:section["end"]])
+    for section in data[0x00]["sections"]:
+        sections.append(data[0x00]["data"][section["start"] : section["end"]])
 
-  i = 0
-  for section in sections:
-    with open(os.path.join(out_dir, f"DATABLK{i}.bin"), "wb") as f:
-      f.write(section)
-  
-  print(f"Saved all data blocks to {out_dir} :3")
+    i = 0
+    for section in sections:
+        with open(os.path.join(out_dir, f"DATABLK{i}.bin"), "wb") as f:
+            f.write(section)
+
+    print(f"Saved all data blocks to {out_dir} :3")
 
 
 def dump_fm_instruments(commands: list[dict[str, Any]], out_dir: str) -> None:
-  ym : YM2612 = YM2612()
-  
-  NO_CHANGE : list[str] = ["n" for _ in range(6)]
+    ym: YM2612 = YM2612()
 
-  instruments : list[YM2612Instrument] = []
-  i : int = 0
-  c : int = 0
-  for cmd in commands:
-    if cmd["cmd"] == "ym_write":
-      result : dict[str, Any] = ym.handle_write_command(cmd)
+    NO_CHANGE: list[str] = ["n" for _ in range(6)]
 
-      if result["notes"] != NO_CHANGE:
-        if DEBUG:
-          print(result["notes"])
-        for channel in range(6):
-          if result["notes"][channel] == "d":
-            inst : YM2612Instrument = YM2612Instrument.from_channel(ym.channels[channel], ym.lfo_freq)
-            idx : int = -1
-            j : int = 0
-            for inst2 in instruments:
-              if YM2612Instrument.compare(inst, inst2):
-                idx = j
-                break
-              j += 1
+    instruments: list[YM2612Instrument] = []
+    i: int = 0
+    c: int = 0
+    for cmd in commands:
+        if cmd["cmd"] == "ym_write":
+            result: dict[str, Any] = ym.handle_write_command(cmd)
 
-            if idx == -1:
-              key_on_ops : dict[str, bool] | None = None
+            if result["notes"] != NO_CHANGE:
+                if DEBUG:
+                    print(result["notes"])
+                for channel in range(6):
+                    if result["notes"][channel] == "d":
+                        inst: YM2612Instrument = YM2612Instrument.from_channel(
+                            ym.channels[channel], ym.lfo_freq
+                        )
+                        idx: int = -1
+                        j: int = 0
+                        for inst2 in instruments:
+                            if YM2612Instrument.compare(inst, inst2):
+                                idx = j
+                                break
+                            j += 1
 
-              if cmd["cmd"] == "ym_write":
-                if cmd["reg"] == 0x28:
-                  key_on_ops = {
-                    "0": cmd["val"] & 0x80 == 0x80,
-                    "1": cmd["val"] & 0x40 == 0x40,
-                    "2": cmd["val"] & 0x20 == 0x20,
-                    "3": cmd["val"] & 0x10 == 0x10,
-                  }
+                        if idx == -1:
+                            key_on_ops: dict[str, bool] | None = None
 
-              inst.metadata = {
-                "all_uses": [
-                  {
-                    "at_command": c,
-                    "at_sample": i,
-                    "key_on_ops": key_on_ops
-                  }
-                ]
-              }
-              instruments.append(inst)
-            else:
-              key_on_ops : dict[str, bool] | None = None
+                            if cmd["cmd"] == "ym_write":
+                                if cmd["reg"] == 0x28:
+                                    key_on_ops = {
+                                        "0": cmd["val"] & 0x80 == 0x80,
+                                        "1": cmd["val"] & 0x40 == 0x40,
+                                        "2": cmd["val"] & 0x20 == 0x20,
+                                        "3": cmd["val"] & 0x10 == 0x10,
+                                    }
 
-              if cmd["cmd"] == "ym_write":
-                if cmd["reg"] == 0x28:
-                  key_on_ops = {
-                    "0": cmd["val"] & 0x80 == 0x80,
-                    "1": cmd["val"] & 0x40 == 0x40,
-                    "2": cmd["val"] & 0x20 == 0x20,
-                    "3": cmd["val"] & 0x10 == 0x10,
-                  }
+                            inst.metadata = {
+                                "all_uses": [
+                                    {
+                                        "at_command": c,
+                                        "at_sample": i,
+                                        "key_on_ops": key_on_ops,
+                                    }
+                                ]
+                            }
+                            instruments.append(inst)
+                        else:
+                            key_on_ops: dict[str, bool] | None = None
 
-              instruments[idx].metadata["all_uses"].append(
-                {
-                  "at_command": c,
-                  "at_sample": i,
-                  "key_on_ops": key_on_ops
-                }
-              )
-              
+                            if cmd["cmd"] == "ym_write":
+                                if cmd["reg"] == 0x28:
+                                    key_on_ops = {
+                                        "0": cmd["val"] & 0x80 == 0x80,
+                                        "1": cmd["val"] & 0x40 == 0x40,
+                                        "2": cmd["val"] & 0x20 == 0x20,
+                                        "3": cmd["val"] & 0x10 == 0x10,
+                                    }
 
-      i += result["advance"]
-      c += 1
+                            instruments[idx].metadata["all_uses"].append(
+                                {
+                                    "at_command": c,
+                                    "at_sample": i,
+                                    "key_on_ops": key_on_ops,
+                                }
+                            )
 
-  print(f"Found {len(instruments)} unique used FM instruments")
+            i += result["advance"]
+            c += 1
 
-  try:
-    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
-  except Exception as e:
-    raise IOError(f"Unable to create FM instrument output folder: {e}")
+    print(f"Found {len(instruments)} unique used FM instruments")
 
-  i : int = -1
-  for inst in instruments:
-    i += 1
-    with open(os.path.join(out_dir, f"INST{i}.json"), "w", encoding="utf-8") as f:
-      json.dump(inst.serialize(), f, indent="\t")
-  
-  print(f"Saved all FM instruments as JSON files to {out_dir} :3")
+    try:
+        pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        raise IOError(f"Unable to create FM instrument output folder: {e}")
 
+    i: int = -1
+    for inst in instruments:
+        i += 1
+        with open(os.path.join(out_dir, f"INST{i}.json"), "w", encoding="utf-8") as f:
+            json.dump(inst.serialize(), f, indent="\t")
+
+    print(f"Saved all FM instruments as JSON files to {out_dir} :3")
 
 
 def main():
-  argparser : argparse.ArgumentParser = argparse.ArgumentParser(
-    prog="mdfmvgmrip.py",
-    description="Sega MegaDrive/Genesis FM Instrument (and more) .VGM Ripper"
-  )
-  argparser.add_argument("filename", help="Path to MegaDrive/Genesis .VGM/.VGZ file")
-  argparser.add_argument("--fm-inst-out", help="Name of folder to save FM instument data to")
-  argparser.add_argument("--midi-out", help="Name of file to save MIDI note data to")
-  argparser.add_argument("--data-out", help="Name of folder to save data block info to (usually samples)")
-  argparser.add_argument("--dump-commands", action="store_true", help="Save all commands to a .TXT file (debug)")
-  argparser.add_argument("--debug", action="store_true", help="Enable debug prints")
-
-  args : argparse.Namespace = argparser.parse_args()
-  # TODO: Check that at least 1 output is specified before reading .VGM
-
-  file_data : bytes = b""
-  try:
-    with open(args.filename, "rb") as f:
-      file_data = f.read()
-  except FileNotFoundError:
-    argparser.error(f'File "{args.filename}" not found')
-    exit(1)
-  except Exception as e:
-    argparser.error(f'Error opening/reading file "{args.filename}": {e}')
-    exit(1)
-  
-  if args.filename.split(".")[-1] == "vgz":
-    file_data = gzip.decompress(file_data)
-
-  warnings.simplefilter("always")
-
-  try:
-    vgm : VGM = VGM.from_data(
-      file_data, 
-      dump_cmds_to=(args.filename + "-writes.txt") if args.dump_commands else None
+    argparser: argparse.ArgumentParser = argparse.ArgumentParser(
+        prog="mdfmvgmrip.py",
+        description="Sega MegaDrive/Genesis FM Instrument (and more) .VGM Ripper",
     )
+    argparser.add_argument("filename", help="Path to MegaDrive/Genesis .VGM/.VGZ file")
+    argparser.add_argument(
+        "--fm-inst-out", help="Name of folder to save FM instument data to"
+    )
+    argparser.add_argument("--midi-out", help="Name of file to save MIDI note data to")
+    argparser.add_argument(
+        "--data-out", help="Name of folder to save data block info to (usually samples)"
+    )
+    argparser.add_argument(
+        "--dump-commands",
+        action="store_true",
+        help="Save all commands to a .TXT file (debug)",
+    )
+    argparser.add_argument("--debug", action="store_true", help="Enable debug prints")
 
-    # TODO: GD3 tag stuff
-    print(f"Read {len(vgm.commands)} commands from .VGM file")
+    args: argparse.Namespace = argparser.parse_args()
+    # TODO: Check that at least 1 output is specified before reading .VGM
 
-    if args.data_out:
-      try:
-        print(f"Found {len(vgm.data[0x00]["sections"])} dumpable data blocks")
-        dump_datablocks(vgm.data, args.data_out)
-      except KeyError:
-        print(".VGM file doesn't contain any dumpable data blocks")
-      except Exception as e:
-        argparser.error(f"Could not save data blocks: {e.__class__.__name__}: {e}")
+    file_data: bytes = b""
+    try:
+        with open(args.filename, "rb") as f:
+            file_data = f.read()
+    except FileNotFoundError:
+        argparser.error(f'File "{args.filename}" not found')
         exit(1)
-    
-    if args.fm_inst_out:
-      dump_fm_instruments(vgm.commands, args.fm_inst_out)
-  except VGMError as e:
-    argparser.error(f"Error parsing VGM file: {e.info}")
-    exit(1)
-  except YM2612Error as e:
-    argparser.error(f"YM2612 error occured: {e.info}")
-    exit(1)
-  except IOError as e:
-    argparser.error(f"Unable to create file/folder: {e}")
+    except Exception as e:
+        argparser.error(f'Error opening/reading file "{args.filename}": {e}')
+        exit(1)
+
+    if args.filename.split(".")[-1] == "vgz":
+        file_data = gzip.decompress(file_data)
+
+    warnings.simplefilter("always")
+
+    try:
+        vgm: VGM = VGM.from_data(
+            file_data,
+            dump_cmds_to=(
+                (args.filename + "-writes.txt") if args.dump_commands else None
+            ),
+        )
+
+        # TODO: GD3 tag stuff
+        print(f"Read {len(vgm.commands)} commands from .VGM file")
+
+        if args.data_out:
+            try:
+                print(f"Found {len(vgm.data[0x00]["sections"])} dumpable data blocks")
+                dump_datablocks(vgm.data, args.data_out)
+            except KeyError:
+                print(".VGM file doesn't contain any dumpable data blocks")
+            except Exception as e:
+                argparser.error(
+                    f"Could not save data blocks: {e.__class__.__name__}: {e}"
+                )
+                exit(1)
+
+        if args.fm_inst_out:
+            dump_fm_instruments(vgm.commands, args.fm_inst_out)
+    except VGMError as e:
+        argparser.error(f"Error parsing VGM file: {e.info}")
+        exit(1)
+    except YM2612Error as e:
+        argparser.error(f"YM2612 error occured: {e.info}")
+        exit(1)
+    except IOError as e:
+        argparser.error(f"Unable to create file/folder: {e}")
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- vgm.py
+++ vgm.py
@@ -2,6 +2,19 @@ import io
 import struct
 import warnings
 from typing import Any
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class Chip(Enum):
+    UNKNOWN = 0
+    YM2612 = 1
+    SEGA_PSG = 2
+
+
+class WriteType(Enum):
+    REGULAR = 0
+    SEGA_PSG_STEREO = 1
 
 
 class VGMError(Exception):
@@ -13,18 +26,67 @@ class VGMError(Exception):
         return f"{self.__class__.__name__}: {self.info}"
 
 
+class VGMCommand(ABC):
+    def __init__(self) -> None:
+        pass
+
+
+class WriteCommand(VGMCommand):
+    def __init__(
+        self,
+        *args,
+        chip: Chip = Chip.UNKNOWN,
+        port: int = -1,
+        register: int = -1,
+        value: int = -1,
+        type: WriteType = WriteType.REGULAR,
+    ) -> None:
+        super().__init__()
+        self.chip: Chip = chip
+        self.port: int = port
+        self.register: int = register
+        self.value: int = value
+
+
+class WaitCommand(VGMCommand):
+    def __init__(self, wait_time: int) -> None:
+        super().__init__()
+        self.wait_time: int = wait_time
+
+
+class YM2612DatabankWriteCommand(WaitCommand):
+    def __init__(self, wait_time: int) -> None:
+        super().__init__(wait_time)
+
+
+class DataBlockSection:
+    def __init__(self, start: int, end: int) -> None:
+        self.start: int = start
+        self.end: int = end
+
+
+class YM2612DatabankSeekCommand(VGMCommand):
+    def __init__(self, seek_address: int) -> None:
+        super().__init__()
+        self.seek_address: int = seek_address
+
+
 class VGM:
     def __init__(self) -> None:
-        # SEGA PSG, YM2612
         self.version: int = -1
-        self.commands: list[dict[str, Any]] = []
-        self.psg_clock: int = 0
-        self.ym2612_clock: int = 0
+        self.commands: list[VGMCommand] = []
         self.gd3_tag: dict[str, str] | None = None
-        self.data: dict = {}
+        self.dblock_data: bytes = b""
+        self.dblock_sections: list[DataBlockSection] = []
+
+        self.sega_psg_clock: int = 0
+        self.ym2612_clock: int = 0
+
+    def is_a_genesis_vgm(self) -> bool:
+        return self.sega_psg_clock != 0 and self.ym2612_clock != 0
 
     @staticmethod
-    def from_data(file_data: bytes, *args, dump_cmds_to: str | None = None) -> VGM:
+    def from_data(file_data: bytes) -> VGM:
         vgm = VGM()
 
         data: io.BytesIO = io.BytesIO(file_data)
@@ -36,26 +98,14 @@ class VGM:
         vgm.version = struct.unpack("<I", data.read(4))[0]
 
         data.seek(0x2C)
-        ym2612_clock: int = struct.unpack("<I", data.read(4))[0]
-        if ym2612_clock > 5000000:
+        vgm.ym2612_clock = struct.unpack("<I", data.read(4))[0]
+        if vgm.ym2612_clock > 5000000:
             if vgm.version <= 0x00000101:
                 data.seek(0x30)
-                ym2612_clock = struct.unpack("<I", data.read(4))[0]
-
-        if ym2612_clock == 0:
-            warnings.warn("YM2612 is not used in this file")
-        elif (ym2612_clock & 0x80 == 0x80) & vgm.version >= 0x00000150:
-            warnings.warn("YM3438 is used (bit 31 is set in YM2612 clock rate)")
+                vgm.ym2612_clock = struct.unpack("<I", data.read(4))[0]
 
         data.seek(0x28)
-        psg_clock: int = struct.unpack("<I", data.read(4))[0]
-        if psg_clock == 0:
-            warnings.warn("SEGA PSG (TI SN76489) is not used in this file")
-
-        if ym2612_clock == 0 and psg_clock == 0:
-            raise VGMError(
-                "Not a Sega MegaDrive/Genesis .VGM (PSG&YM2612 are not used)"
-            )
+        vgm.sega_psg_clock = struct.unpack("<I", data.read(4))[0]
 
         data_offset: int = 0x0C
         if vgm.version >= 0x00000150:
@@ -65,85 +115,54 @@ class VGM:
         data.seek(0x34 + data_offset)
         i = 0x34 + data_offset - 1
 
-        cmddump: io.TextIOWrapper | None = None
-        if type(dump_cmds_to) is str:
-            cmddump = open(dump_cmds_to, "w")
-
         while True:
             i += 1
             cmd = data.read(1)[0]
 
             if cmd == 0x66:  # end of VGM data stream
-                if cmddump:
-                    cmddump.write("End of VGM data stream [0x66]\n")
                 break
             elif cmd == 0x67:  # data block
                 data_block = VGM.parse_data_block(data, i)
-                if not data_block["type"] in vgm.data.keys():
-                    vgm.data[data_block["type"]] = {
-                        "data": data_block["data"],
-                        "sections": [{"start": 0, "end": len(data_block["data"])}],
-                    }
-                else:
-                    vgm.data[data_block["type"]]["data"] += data_block["data"]
-                    vgm.data[data_block["type"]]["sections"].append(
-                        {
-                            "start": len(vgm.data[data_block["type"]]["data"]) + 1,
-                            "end": len(vgm.data[data_block["type"]]["data"])
-                            + 1
-                            + len(data_block["data"]),
-                        }
+                vgm.dblock_sections.append(
+                    DataBlockSection(
+                        len(vgm.dblock_data),
+                        len(vgm.dblock_data) + len(data_block["data"]),
                     )
-                i += 2 + 4 + len(vgm.data)
-
-                if cmddump:
-                    cmddump.write(
-                        f"Data block [0x67]: size {len(data_block['data'])}, type {data_block['type']}\n"
-                    )
+                )
+                vgm.dblock_data += data_block["data"]
+                i += 2 + 4 + len(data_block["data"])
             elif cmd == 0x68:  # pcm ram write (stub)
                 raise VGMError(f"At byte {i}: PCM RAM write (unimplemented)")
             elif 0x90 <= cmd <= 0x95:  # dac stream control (stub)
-                if cmddump:
-                    cmddump.write(
-                        f"DAC stream control command [0x90-0x95, {cmd}] - UNIMPLEMENTED\n"
-                    )
-                    cmddump.close()
                 raise VGMError(f"At byte {i}: DAC stream control write (unimplemented)")
             elif 0x80 <= cmd <= 0x8F:  # YM2612 port 0 addr 2a write from databank
                 wait_time: int = cmd - 0x80
-                if cmddump:
-                    cmddump.write(
-                        f"YM2612 write from databank and wait {wait_time} samples [0x8n, {cmd}]\n"
-                    )
-                vgm.commands.append({"cmd": "ym_dbank_write", "wait": wait_time})
+                vgm.commands.append(YM2612DatabankWriteCommand(wait_time))
             elif cmd == 0x52 or cmd == 0x53:  # YM2612 write
                 port: int = max(0, cmd - 0x52)
                 register: int = data.read(1)[0]
                 value: int = data.read(1)[0]
-                if cmddump:
-                    cmddump.write(
-                        f"YM2612 write [0x52/0x53] - port {port}, reg {register}, val {value} ({bin(value)})\n"
-                    )
                 i += 2
                 vgm.commands.append(
-                    {"cmd": "ym_write", "port": port, "reg": register, "val": value}
+                    WriteCommand(
+                        chip=Chip.YM2612, port=port, register=register, value=value
+                    )
                 )
             elif cmd == 0x4F:  # PSG port 0x06 write
                 value: int = data.read(1)[0]
-                if cmddump:
-                    cmddump.write(
-                        f"SEGA PSG (SN76489) stereo write - port 0x06, val {value} ({bin(value)})\n"
-                    )
                 i += 1
-                vgm.commands.append({"cmd": "psg_stereo_write", "val": value})
+                vgm.commands.append(
+                    WriteCommand(
+                        chip=Chip.SEGA_PSG,
+                        type=WriteType.SEGA_PSG_STEREO,
+                        port=0x06,
+                        value=value,
+                    )
+                )
             elif cmd == 0x50:  # PSG write
                 value: int = data.read(1)[0]
-                if cmddump:
-                    cmddump.write(
-                        f"SEGA PSG (SN76489) write - val {value} ({bin(value)})\n"
-                    )
                 i += 1
-                vgm.commands.append({"cmd": "psg_write", "val": value})
+                vgm.commands.append(WriteCommand(chip=Chip.SEGA_PSG, value=value))
             elif (0x61 <= cmd <= 0x63) or (0x70 <= cmd <= 0x7F):  # wait...
                 wait_time: int = -1
                 if cmd == 0x61:  # nnnn samples
@@ -155,27 +174,12 @@ class VGM:
                 else:  # n+1 samples, where n is the lower nibble of the command
                     wait_time = cmd - 0x70 + 1
 
-                if cmddump:
-                    cmddump.write(f"Wait {wait_time} samples [{cmd}]\n")
-                vgm.commands.append({"cmd": "wait", "wait": wait_time})
+                vgm.commands.append(WaitCommand(wait_time))
             elif cmd == 0xE0:  # seek to offset dddddddd in YM2612 (type 0x00) databank
                 dbank_offset: int = struct.unpack("<I", data.read(4))[0]
-                if cmddump:
-                    cmddump.write(
-                        f"Seek to offset {dbank_offset} in YM2612 PCM databank [0xE0]\n"
-                    )
-                vgm.commands.append({"cmd": "ym_dbank_seek", "seek": dbank_offset})
+                vgm.commands.append(YM2612DatabankSeekCommand(dbank_offset))
             else:  # unknown cmds
-                if cmddump:
-                    cmddump.write(f"UNKNOWN COMMAND {cmd} (unimplemented)\n")
-                    cmddump.close()
-                raise VGMError(
-                    f"At byte {i}: Unknown command - wat da hell is {cmd}?!?!?!?"
-                )
-
-        if cmddump:
-            print(f"Commands dumped to {dump_cmds_to}")
-            cmddump.close()
+                raise VGMError(f"At byte {i}: Unknown command {cmd}")
 
         return vgm
 
